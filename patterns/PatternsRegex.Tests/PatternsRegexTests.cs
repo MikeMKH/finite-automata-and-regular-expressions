cd1e301c562223b2140ea5235acd951531395dfb
Mike Harris <MikeMKH@users.noreply.github.com>
2023-06-23 06:07:00 -0500

    add problem 19

--- patterns/PatternsRegex.Tests/PatternsRegexTests.cs
+++ patterns/PatternsRegex.Tests/PatternsRegexTests.cs
@@ -78,4 +78,30 @@ public class PatternsRegexTests
     [InlineData("4311.o", false)]
     public void base10Decimal_GivenStringMatchTheExpectedResult(string given, bool expected)
       => Assert.Equal(expected, Regex.IsMatch(given, base10Decimal));
+      
+    /*
+      problem 19
+      binary number with exactly three 1s
+    */
+    
+    const string binaryExactlyThee1 = @"^(0*10*){3}$";
+    
+    [Theory]
+    [InlineData("111", true)]
+    [InlineData("0101010", true)]
+    [InlineData("000010001000010000000000", true)]
+    [InlineData("0000110001", true)]
+    [InlineData("111000", true)]
+    [InlineData("01110", true)]
+    [InlineData("011101", false)]
+    [InlineData("011102", false)]
+    [InlineData("111.", false)]
+    [InlineData("11.1", false)]
+    [InlineData("hello", false)]
+    [InlineData("he1110", false)]
+    [InlineData("h3110", false)]
+    [InlineData("4311o", false)]
+    [InlineData("4311.o", false)]
+    public void binaryExactlyThee1_GivenStringMatchTheExpectedResult(string given, bool expected)
+      => Assert.Equal(expected, Regex.IsMatch(given, binaryExactlyThee1));
 }
