020716bc979ab6f0ff829cf0456f50cb9a715d6a
Mike Harris <MikeMKH@users.noreply.github.com>
2023-06-28 06:11:32 -0500

    init

---
+++ 3bit/ThreeBitRegex.Tests/ThreeBitRegexTests.cs
@@ -0,0 +1,12 @@
+using System.Text.RegularExpressions;
+
+namespace ThreeBitRegex.Tests;
+
+public class ThreeBitRegexTests
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(8==16/2);
+    }
+}
