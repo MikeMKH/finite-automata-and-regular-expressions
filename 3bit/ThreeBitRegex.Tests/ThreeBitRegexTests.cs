9ca877c9e2e0db1a12d914774964637e70944f9d
Mike Harris <MikeMKH@users.noreply.github.com>
2023-06-29 06:08:35 -0500

    add problem 30

--- 3bit/ThreeBitRegex.Tests/ThreeBitRegexTests.cs
+++ 3bit/ThreeBitRegex.Tests/ThreeBitRegexTests.cs
@@ -27,5 +27,30 @@ public class ThreeBitRegexTests
     [InlineData("4311.o", false)]
     public void binary000OnceAtEnd_GivenStringMatchTheExpectedResult(string given, bool expected)
       => Assert.Equal(expected, Regex.IsMatch(given, binary000OnceAtEnd));
+      
+    /*
+      problem 30
+      binary number with 000 occurring once at the end and anywhere else
+    */
+    const string binary000AtEnd = @"^(1|(0(1|(0(1|(00*1))))))*0000*$";
+    
+    [Theory]
+    [InlineData("000", true)]
+    [InlineData("1000", true)]
+    [InlineData("101000", true)]
+    [InlineData("01000", true)]
+    [InlineData("101001111011000", true)]
+    [InlineData("0000", true)]
+    [InlineData("0001000", true)]
+    [InlineData("10100111101100010001000", true)]
+    [InlineData("111.", false)]
+    [InlineData("11.1", false)]
+    [InlineData("hello", false)]
+    [InlineData("he1110", false)]
+    [InlineData("h3110", false)]
+    [InlineData("4311o", false)]
+    [InlineData("4311.o", false)]
+    public void binary000AtEnd_GivenStringMatchTheExpectedResult(string given, bool expected)
+      => Assert.Equal(expected, Regex.IsMatch(given, binary000AtEnd));
     
 }
