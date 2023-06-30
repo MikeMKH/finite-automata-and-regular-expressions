e6bc70f7c75c75cc544d7a82d59bf5c28634571e
Mike Harris <MikeMKH@users.noreply.github.com>
2023-06-30 06:14:15 -0500

    init

---
+++ 4bit/FourBitRegex.Tests/FourBitRegexTests.cs
@@ -0,0 +1,12 @@
+using System.Text.RegularExpressions;
+
+namespace FourBitRegex.Tests;
+
+public class FourBitRegexTests
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(8==2*2*2);
+    }
+}
