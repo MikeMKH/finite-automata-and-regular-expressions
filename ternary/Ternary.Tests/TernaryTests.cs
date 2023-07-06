a0a9c946c636a35bbaa3bf07f5010ce04e4927bb
Mike Harris <MikeMKH@users.noreply.github.com>
2023-07-06 06:06:21 -0500

    init

---
+++ ternary/Ternary.Tests/TernaryTests.cs
@@ -0,0 +1,12 @@
+using System.Text.RegularExpressions;
+
+namespace Ternary.Tests;
+
+public class TernaryTests
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(8==2*2*2);
+    }
+}
