73b10791423ab457477aadf79638853144e6d473
Mike Harris <MikeMKH@users.noreply.github.com>
2023-07-07 06:09:07 -0500

    add problem 54

--- ternary/Ternary.Tests/TernaryTests.cs
+++ ternary/Ternary.Tests/TernaryTests.cs
@@ -19,6 +19,12 @@ public class TernaryTests
     [InlineData("0101", false)]
     [InlineData("000010001", false)]
     [InlineData("00001001", false)]
+    [InlineData("00", false)]
+    [InlineData("02", false)]
+    [InlineData("11", false)]
+    [InlineData("12", false)]
+    [InlineData("21", false)]
+    [InlineData("22", false)]
     [InlineData("111.", false)]
     [InlineData("11.1", false)]
     [InlineData("hello", false)]
@@ -29,4 +35,34 @@ public class TernaryTests
     public void ternary01OnceAtEndAtEnd_GivenStringMatchTheExpectedResult(string given, bool expected)
       => Assert.Equal(expected, Regex.IsMatch(given, ternary01OnceAtEnd));
 
+    /*
+      problem 54
+      ternary number with 01 occurring once at the end and anywhere else
+    */
+    const string ternary01AtEnd = @"^([12]|0(0|(10))*(2|(1[12])))*0(0|(10))*1$";
+    
+    [Theory]
+    [InlineData("01", true)]
+    [InlineData("120001", true)]
+    [InlineData("12002101", true)]
+    [InlineData("111110000002222201", true)]
+    [InlineData("212101", true)]
+    [InlineData("0101", true)]
+    [InlineData("000010001", true)]
+    [InlineData("00001001", true)]
+    [InlineData("00", false)]
+    [InlineData("02", false)]
+    [InlineData("11", false)]
+    [InlineData("12", false)]
+    [InlineData("21", false)]
+    [InlineData("22", false)]
+    [InlineData("111.", false)]
+    [InlineData("11.1", false)]
+    [InlineData("hello", false)]
+    [InlineData("he1110", false)]
+    [InlineData("h3110", false)]
+    [InlineData("4311o", false)]
+    [InlineData("4311.o", false)]
+    public void ternary01AtEndAtEnd_GivenStringMatchTheExpectedResult(string given, bool expected)
+      => Assert.Equal(expected, Regex.IsMatch(given, ternary01AtEnd));
 }
