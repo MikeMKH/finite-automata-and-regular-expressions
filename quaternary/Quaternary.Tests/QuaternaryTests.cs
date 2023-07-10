df17191ce3d61a948e4ba2de4a0a518178ecfab4
Mike Harris <MikeMKH@users.noreply.github.com>
2023-07-10 06:11:19 -0500

    add problem 60

--- quaternary/Quaternary.Tests/QuaternaryTests.cs
+++ quaternary/Quaternary.Tests/QuaternaryTests.cs
@@ -41,4 +41,43 @@ public class QuaternaryTests
     [InlineData("4311.o", false)]
     public void quaternary01OnceAtEnd_GivenStringMatchTheExpectedResult(string given, bool expected)
       => Assert.Equal(expected, Regex.IsMatch(given, quaternary01OnceAtEnd));
+
+    /*
+      problem 60
+      quaternary number with 01 occurring once at the end and anywhere else
+    */
+    const string quaternary01AtEnd = @"^([123]|0(0|(10))*(3|(2|(1[123]))))*0(0|(10))*1$";
+    
+    [Theory]
+    [InlineData("01", true)]
+    [InlineData("1230001", true)]
+    [InlineData("1230032101", true)]
+    [InlineData("11111000000222220000003333301", true)]
+    [InlineData("32132101", true)]
+    [InlineData("0101", true)]
+    [InlineData("000010001", true)]
+    [InlineData("00001001", true)]
+    [InlineData("00", false)]
+    [InlineData("02", false)]
+    [InlineData("03", false)]
+    [InlineData("11", false)]
+    [InlineData("12", false)]
+    [InlineData("13", false)]
+    [InlineData("21", false)]
+    [InlineData("22", false)]
+    [InlineData("23", false)]
+    [InlineData("30", false)]
+    [InlineData("31", false)]
+    [InlineData("32", false)]
+    [InlineData("33", false)]
+    [InlineData("111.", false)]
+    [InlineData("11.1", false)]
+    [InlineData("hello", false)]
+    [InlineData("he1110", false)]
+    [InlineData("h3110", false)]
+    [InlineData("4311o", false)]
+    [InlineData("4311.o", false)]
+    public void quaternary01AtEnd_GivenStringMatchTheExpectedResult(string given, bool expected)
+      => Assert.Equal(expected, Regex.IsMatch(given, quaternary01AtEnd));
+
 }
