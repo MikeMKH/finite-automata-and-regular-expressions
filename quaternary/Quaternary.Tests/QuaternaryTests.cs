042553a75cdef95b2b89044a55e245731e6f7c73
Mike Harris <MikeMKH@users.noreply.github.com>
2023-07-10 06:03:33 -0500

    init

---
+++ quaternary/Quaternary.Tests/QuaternaryTests.cs
@@ -0,0 +1,12 @@
+using System.Text.RegularExpressions;
+
+namespace Quaternary.Tests;
+
+public class QuaternaryTests
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(8==2<<2);
+    }
+}
