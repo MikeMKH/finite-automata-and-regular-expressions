c4cc3331cf093cc694592747a474a39a7fbc7f7a
Mike Harris <MikeMKH@users.noreply.github.com>
2023-07-11 06:21:00 -0500

    add problem 65

--- circular/Circular.Tests/CircularTests.cs
+++ circular/Circular.Tests/CircularTests.cs
@@ -4,9 +4,30 @@ namespace Circular.Tests;
 
 public class CircularTests
 {
-    [Fact]
-    public void Test1()
-    {
-        Assert.True(8 == 32>>2);
-    }
+    /*
+      problem 65
+      circular string over the alphabet {0,1} that do not contain 00
+    */
+    const string circularString00NotContain = @"^0?1(1|(01))*0?$";
+    
+    [Theory]
+    [InlineData("01", true)]
+    [InlineData("11", true)]
+    [InlineData("10", true)]
+    [InlineData("1010101010101010101", true)]
+    [InlineData("01110111011110", true)]
+    [InlineData("01001", false)]
+    [InlineData("001", false)]
+    [InlineData("1100", false)]
+    [InlineData("00", false)]
+    [InlineData("111.", false)]
+    [InlineData("11.1", false)]
+    [InlineData("hello", false)]
+    [InlineData("he1110", false)]
+    [InlineData("h3110", false)]
+    [InlineData("4311o", false)]
+    [InlineData("4311.o", false)]
+    public void circularString00NotContain_GivenStringMatchTheExpectedResult(string given, bool expected)
+      => Assert.Equal(expected, Regex.IsMatch(given, circularString00NotContain));
+
 }
